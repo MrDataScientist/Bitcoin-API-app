a1ba9b5b8c4d1e5ac855e4c9788e798d84fcb509
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-10-22 18:53:23 +0200

    wdefgerg

---
+++ JS training/test1.html.js
@@ -0,0 +1,16 @@
+<html>
+  <head>
+  <title>Creating object JSON with JavaScript</title>
+  <script language = "javascript">
+    var JSONobj = {"name":"tarik","company":"cloudeo","course":"JSON crash course","year":"2017"};
+
+    document.write("<h1>testing ...</h1>");
+    document.write("<br>");
+    document.write("<h3>hi, my name is"+JSONobj.name+"and I am teaching +JSONobj.course" "</h3>");
+    document.write("<h3>"+JSONobj.year+ "is a wonderful year </h3>");
+    document.write("<h4> Yes, it worked!!! </h4>");
+
+  </script>
+  </head>
+
+</html>
